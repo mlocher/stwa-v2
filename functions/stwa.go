fb3db4e84a8d80d1be31365aac4623b0df53c946
Marko Locher <marko@Betelgeuse.stmk.ooble.io>
2020-03-22 11:03:23 +0100

    Add additional metadata to the gale warning geoJSON

--- functions/stwa.go
+++ functions/stwa.go
@@ -53,8 +53,10 @@ type gjFeature struct {
 }
 
 type gjProperties struct {
-	Name  string `json:"name"`
-	State string `json:"state"`
+	Name        string `json:"name"`
+	State       string `json:"state"`
+	Description string `json:"description,omitempty"`
+	Height      int    `json:"height,omitempty"`
 }
 
 type gjGeometry struct {
@@ -62,7 +64,7 @@ type gjGeometry struct {
 	Coordinates [2]float32 `json:"coordinates"`
 }
 
-func newFeature(name string, state string, coordinates [2]float32) gjFeature {
+func newFeature(name string, state string, coordinates [2]float32, description string, height int) gjFeature {
 	f := gjFeature{
 		Type:       "Feature",
 		Properties: gjProperties{},
@@ -72,6 +74,8 @@ func newFeature(name string, state string, coordinates [2]float32) gjFeature {
 	}
 	f.Properties.Name = name
 	f.Properties.State = state
+	f.Properties.Description = description
+	f.Properties.Height = height
 	f.Geometry.Coordinates = coordinates
 	return f
 }
@@ -85,18 +89,79 @@ func newFeatureCollection(features []gjFeature) gjFeatureCollection {
 }
 
 func stwa(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	stwaCoordinates := map[string][2]float32{
-		"Neusiedl":       [2]float32{16.83833333, 47.92666667},
-		"Weiden":         [2]float32{16.85222222, 47.91666667},
-		"Podersdorf":     [2]float32{16.82638889, 47.86027778},
-		"Illmitz-Hoelle": [2]float32{16.78305556, 47.80361111},
-		"Illmitz":        [2]float32{16.7425, 47.75583333},
-		"Fertoerakos":    [2]float32{16.69333333, 47.72027778},
-		"Moerbisch":      [2]float32{16.69777778, 47.75138889},
-		"Rust":           [2]float32{16.69944444, 47.80361111},
-		"Oggau":          [2]float32{16.70111111, 47.845},
-		"Purbach":        [2]float32{16.74611111, 47.90111111},
-		"Breitenbrunn":   [2]float32{16.77, 47.91611111},
+	type stwaMetaData struct {
+		name        string
+		description string
+		height      int
+		coordinates [2]float32
+	}
+	metaData := map[string]stwaMetaData{
+		"Neusiedl": stwaMetaData{
+			name:        "Neusiedl",
+			description: "Mast auf der Mole im Südhafen",
+			height:      6,
+			coordinates: [2]float32{16.83833333, 47.92666667},
+		},
+		"Weiden": stwaMetaData{
+			name:        "Weiden",
+			description: "Mast Kanaleinfahrt, von See kommend links",
+			height:      0,
+			coordinates: [2]float32{16.85222222, 47.91666667},
+		},
+		"Podersdorf": stwaMetaData{
+			name:        "Podersdorf",
+			description: "Leuchtturm an der Außenmole",
+			height:      0,
+			coordinates: [2]float32{16.82638889, 47.86027778},
+		},
+		"Illmitz-Hoelle": stwaMetaData{
+			name:        "Illmitz-Hölle",
+			description: "Mast am Haus des Wasserleitungsverbandes",
+			height:      7,
+			coordinates: [2]float32{16.78305556, 47.80361111},
+		},
+		"Illmitz": stwaMetaData{
+			name:        "Illmitz",
+			description: "Mast in der Badeanlage - Gemeindehafen, südlich des Yachtclub Illmitz (YCI)",
+			height:      0,
+			coordinates: [2]float32{16.7425, 47.75583333},
+		},
+		"Fertoerakos": stwaMetaData{
+			name:        "Fertörakos",
+			description: "Mast am Haus des Wasserverbandes",
+			height:      0,
+			coordinates: [2]float32{16.69333333, 47.72027778},
+		},
+		"Moerbisch": stwaMetaData{
+			name:        "Mörbisch",
+			description: "Am Mast des Polizei- und Zollwachhauses",
+			height:      8,
+			coordinates: [2]float32{16.69777778, 47.75138889},
+		},
+		"Rust": stwaMetaData{
+			name:        "Rust",
+			description: "Turm mit Mast am Clubhaus des Burgenländischen Yachtclubs (BYC)",
+			height:      10,
+			coordinates: [2]float32{16.69944444, 47.80361111},
+		},
+		"Oggau": stwaMetaData{
+			name:        "Oggau",
+			description: "Mast bei Kanaleinfahrt",
+			height:      20,
+			coordinates: [2]float32{16.70111111, 47.845},
+		},
+		"Purbach": stwaMetaData{
+			name:        "Purbach",
+			description: "Mast am Fußwegende, Anfang Kanaleinfahrt",
+			height:      5,
+			coordinates: [2]float32{16.74611111, 47.90111111},
+		},
+		"Breitenbrunn": stwaMetaData{
+			name:        "Breitenbrunn",
+			description: "Flaggenmast im Yachtclub Breitenbrunn (YCBb)",
+			height:      15,
+			coordinates: [2]float32{16.77, 47.91611111},
+		},
 	}
 
 	url := "https://www.lsz-b.at/fileadmin/stwa/scripts/holeStwaStatus.php"
@@ -118,7 +183,7 @@ func stwa(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse
 		if site.Name == "Neusiedler See" {
 			continue
 		}
-		sites = append(sites, newFeature(site.Name, site.State, stwaCoordinates[site.Name]))
+		sites = append(sites, newFeature(metaData[site.Name].name, site.State, metaData[site.Name].coordinates, metaData[site.Name].description, metaData[site.Name].height))
 	}
 
 	geoJSON, err := json.Marshal(newFeatureCollection(sites))
