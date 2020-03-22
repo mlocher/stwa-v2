8977ecdf1bd5bfa77c352444ff084bf4b6114849
Marko Locher <marko@Betelgeuse.stmk.ooble.io>
2020-03-22 11:46:29 +0100

    Only run PurgeCSS in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,19 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
+const purgecss = require('@fullhuman/postcss-purgecss')({
+	content: [ `./public/**/*.html`, `./src/**/*.vue` ],
+	defaultExtractor (content) {
+		const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+		return contentWithoutStyleBlocks.match(/[\w-/.:]+(?<!:)/g) || []
+	},
+	whitelist: [],
+	whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/, /mapboxgl.*/ ],
+})
+
 module.exports = {
 	"plugins": [
 		require('tailwindcss')('tailwind.config.js'),
 		require('autoprefixer')(),
-		purgecss({
-			content: [ `./public/**/*.html`, `./src/**/*.vue` ],
-			defaultExtractor (content) {
-				const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
-				return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
-			},
-			whitelist: [],
-			whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/, /mapboxgl.*/ ],
-		})
+		...process.env.NODE_ENV === 'production'
+			? [ purgecss ]
+			: []
 	]
 }
