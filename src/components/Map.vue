ae8050dee3d4bdc4b5fb41506754eae4bdcfb27e
Marko Locher <marko@locher.at>
2020-04-10 08:36:54 +0200

    Update map popups to use TailwindCSS styling (#7)
    * Add additional information (state in both icon and text form)
    * Slightly increase map marker sizes
    * (Temp) remove map labels

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -34,7 +34,8 @@ export default {
 
         this.popup = new mapboxgl.Popup({
             closeButton: false,
-            closeOnClick: false
+            closeOnClick: false,
+            offset: 16
         });
 
         this.map.on('load', () => {
@@ -88,7 +89,7 @@ export default {
                         ['==', ['get', 'state'], 'gale_warning'], tailwind.theme.colors.red['500'],
                         ['==', ['get', 'state'], 'out_of_order'], tailwind.theme.colors.blue['200'],
                         tailwind.theme.colors.gray['200'] ],
-                    'circle-radius': 10
+                    'circle-radius': 14
                 }
             });
 
@@ -104,15 +105,15 @@ export default {
                         ['==', ['get', 'state'], 'out_of_order'], 'stwaMarkerDark',
                         'stwaMarkerDark'],
                     'icon-allow-overlap': true,
-                    'text-field': ['get', 'name'],
-                    'text-size': 10,
-                    'text-variable-anchor': ['left', 'right'],
-                    'text-radial-offset': 1.25,
-                    'text-optional': true
+                    //'text-field': ['get', 'name'],
+                    //'text-size': 10,
+                    //'text-variable-anchor': ['left', 'right'],
+                    //'text-radial-offset': 1.25,
+                    //'text-optional': true
                 },
                 'paint': {
-                    'text-color': tailwind.theme.colors.gray['700'],
-                    'text-halo-blur': 2,
+                    //'text-color': tailwind.theme.colors.gray['700'],
+                    //'text-halo-blur': 2,
                 }
             });
 
@@ -125,6 +126,32 @@ export default {
             var coordinates = e.features[0].geometry.coordinates.slice();
             var name = e.features[0].properties.name;
             var description = e.features[0].properties.description;
+            var stateClasses = null;
+            var stateLabel = null
+
+            switch (e.features[0].properties.state) {
+                case 'standby':
+                    stateClasses = 'bg-gray-200 text-green-500';
+                    stateLabel = 'Bereitschaft';
+                    break;
+                case 'advance_warning':
+                    stateClasses = 'bg-yellow-300 text-gray-500';
+                    stateLabel = 'Vorwarnung';
+                    break;
+                case 'gale_warning':
+                    stateClasses = 'bg-red-500 text-gray-200';
+                    stateLabel = 'Vorwarnung';
+                    break;
+                case 'out_of_order':
+                    stateClasses = 'bg-blue-200 text-gray-500';
+                    stateLabel = 'Außer Betrieb';
+                    break;
+                case 'unknown':
+                default:
+                    stateClasses = 'bg-gray-200 text-gray-500';
+                    stateLabel = 'Unbekannt';
+                    break;
+            }
 
             // Ensure that if the map is zoomed out such that multiple
             // copies of the feature are visible, the popup appears
@@ -135,10 +162,26 @@ export default {
 
             this.popup
                 .setLngLat(coordinates)
-                .setHTML(`<div class="text-gray-500">
-                    <p class="text-gray-700 font-bold mb-2">STWA ${name}</p>
-                    <p class="leading-tight">${description}</p>
-                </div>`)
+                .setHTML(`<div class="sm:flex sm:items-start">
+                    <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full ${stateClasses} sm:mx-0 sm:h-10 sm:w-10">
+                        <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
+                            <path d="M11 3a1 1 0 10-2 0v1a1 1 0 102 0V3zM15.657 5.757a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707zM18 10a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zM5.05 6.464A1 1 0 106.464 5.05l-.707-.707a1 1 0 00-1.414 1.414l.707.707zM5 10a1 1 0 01-1 1H3a1 1 0 110-2h1a1 1 0 011 1zM8 16v-1h4v1a2 2 0 11-4 0zM12 14c.015-.34.208-.646.477-.859a4 4 0 10-4.954 0c.27.213.462.519.476.859h4.002z"></path>
+                        </svg>
+                    </div>
+                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <h3 class="text-lg leading-6 font-medium text-gray-900">
+                            STWA ${name}
+                        </h3>
+                        <div>
+                            <p class="text-xs leading-snug text-gray-500">${stateLabel}</p>
+                        </div>
+                        <div class="mt-2">
+                            <p class="text-xs leading-snug text-gray-500">
+                                ${description}
+                            </p>
+                        </div>
+                    </div>
+                    </div>`)
                 .addTo(this.map);
         })
 
@@ -150,6 +193,10 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 @import "~mapbox-gl/dist/mapbox-gl.css";
+
+.mapboxgl-popup-content {
+    @apply shadow-lg rounded-md;
+}
 </style>
