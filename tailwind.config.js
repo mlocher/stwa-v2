2edcc70bc3f648b9ee7310e39f32e938f4f89503
Marko Locher <marko@locher.at>
2020-08-19 08:32:38 +0200

    Update to Tailwind CSS 1.7.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/ui'),
-  ]
+    variants: {},
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/ui'),
+    ],
+    future: {
+        removeDeprecatedGapUtilities: true,
+    },
+    purge: false
+
 }
